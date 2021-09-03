b7b1ead4e7002bc61677fcd70c16885c4b88b4ba
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-09-03 04:09:29 -0300

    feat: create role validator

---
+++ src/api/validators/RoleValidator.ts
@@ -0,0 +1,55 @@
+import * as Yup from 'yup';
+
+import ApiError from '../../core/exceptions/ApiError';
+import { Request } from 'express';
+
+class RoleValidator {
+  async create(req: Request) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      description: Yup.string().optional(),
+    });
+
+    return schema.validate(req.body).catch(err => {
+      throw new ApiError(400, err.message);
+    });
+  }
+
+  async update(req: Request) {
+    const schema = Yup.object({
+      id: Yup.string()
+        .matches(/^[0-9a-fA-F]{24}$/, 'id must be a ObjectId')
+        .required(),
+      name: Yup.string().required(),
+      description: Yup.string().optional(),
+    });
+
+    return schema.validate({ ...req.params, ...req.body }).catch(err => {
+      throw new ApiError(400, err.message);
+    });
+  }
+
+  async list(req: Request) {
+    const schema = Yup.object({
+      name: Yup.string().optional(),
+    });
+
+    return schema.validate(req.query).catch(err => {
+      throw new ApiError(400, err.message);
+    });
+  }
+
+  async showOrDelete(req: Request) {
+    const schema = Yup.object({
+      id: Yup.string()
+        .matches(/^[0-9a-fA-F]{24}$/, 'id must be a ObjectId')
+        .required(),
+    });
+
+    return schema.validate(req.params).catch(err => {
+      throw new ApiError(400, err.message);
+    });
+  }
+}
+
+export default new RoleValidator();
