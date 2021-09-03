494534faf954dd92d5936b8069069ebdc472c140
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-09-03 04:10:21 -0300

    feat: add role routes to main router

--- src/api/router.ts
+++ src/api/router.ts
@@ -1,11 +1,18 @@
 import IndexController from './controllers/IndexController';
-import ProductRoutes from './routes/productRoutes';
+import productRoutes from './routes/productRoutes';
 import express from 'express';
+import ApiError from '../core/exceptions/ApiError';
+import roleRoutes from './routes/roleRoutes';
 
 const router = express.Router();
 
 router.get('/', IndexController.index);
 
-router.use('/product', ProductRoutes);
+router.use('/product', productRoutes);
+router.use('/role', roleRoutes);
+
+router.all('*', () => {
+  throw new ApiError(404, 'Route not found');
+});
 
 export default router;
