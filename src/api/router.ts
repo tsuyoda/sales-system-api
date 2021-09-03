0625e633c18cd1a7ecc0022adf13654263c6db72
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-09-03 18:20:33 -0300

    feat: add user in main router

--- src/api/router.ts
+++ src/api/router.ts
@@ -3,6 +3,7 @@ import productRoutes from './routes/productRoutes';
 import express from 'express';
 import ApiError from '../core/exceptions/ApiError';
 import roleRoutes from './routes/roleRoutes';
+import userRoutes from './routes/userRoutes';
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ router.get('/', IndexController.index);
 
 router.use('/product', productRoutes);
 router.use('/role', roleRoutes);
+router.use('/user', userRoutes);
 
 router.all('*', () => {
   throw new ApiError(404, 'Route not found');
