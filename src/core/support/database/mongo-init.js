2e79e6f064689f758c902909c8c7d287adaa3411
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-09-21 19:55:39 -0300

    feat: add mongo default init

---
+++ src/core/support/database/mongo-init.js
@@ -0,0 +1,20 @@
+db = new Mongo().getDB('salessystem');
+
+db.createCollection('roles', { capped: false });
+db.createCollection('users', { capped: false });
+
+const role =
+  db.roles.findOne({ name: 'ROLE_ADMIN' }) ||
+  db.roles.insertOne({
+    name: 'ROLE_ADMIN',
+    description: 'Administrador',
+  });
+
+db.users.findOne({ name: 'admin' }) ||
+  db.users.insertOne({
+    name: 'admin',
+    fullName: 'Administrator',
+    email: 'admin@admin.com',
+    password: '$2b$10$LDgghD.HhTxU3pNEqTpHFuUvhKFQv3hc0WtbXmqs5mU8wkpZhBNgG',
+    role: role.insertedId.valueOf(),
+  });
