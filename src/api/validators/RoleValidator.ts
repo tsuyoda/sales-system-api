733f990b645703beb32a8b623cd1eef7aab672d3
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-10-22 01:42:25 -0300

    feat: improve role and user validators

--- src/api/validators/RoleValidator.ts
+++ src/api/validators/RoleValidator.ts
@@ -2,12 +2,24 @@ import * as Yup from 'yup';
 
 import ApiError from '../../core/exceptions/ApiError';
 import { Request } from 'express';
+import { REGEX_OBJECT_ID } from '../../core/constants/regex';
 
 class RoleValidator {
   async create(req: Request) {
     const schema = Yup.object({
       name: Yup.string().required(),
       description: Yup.string().optional(),
+      permissions: Yup.array()
+        .of(
+          Yup.object({
+            resource: Yup.string()
+              .matches(new RegExp(REGEX_OBJECT_ID), 'permission must be a ObjectId')
+              .required(),
+            actions: Yup.array().of(Yup.string().required()).required(),
+          })
+        )
+        .required(),
+      isAdmin: Yup.boolean().optional().default(false),
     });
 
     return schema.validate(req.body).catch(err => {
@@ -17,11 +29,20 @@ class RoleValidator {
 
   async update(req: Request) {
     const schema = Yup.object({
-      id: Yup.string()
-        .matches(/^[0-9a-fA-F]{24}$/, 'id must be a ObjectId')
-        .required(),
+      id: Yup.string().matches(new RegExp(REGEX_OBJECT_ID), 'id must be a ObjectId').required(),
       name: Yup.string().required(),
       description: Yup.string().optional(),
+      permissions: Yup.array()
+        .of(
+          Yup.object({
+            resource: Yup.string()
+              .matches(new RegExp(REGEX_OBJECT_ID), 'permission must be a ObjectId')
+              .required(),
+            actions: Yup.array().of(Yup.string().required()).required(),
+          })
+        )
+        .required(),
+      isAdmin: Yup.boolean().optional(),
     });
 
     return schema.validate({ ...req.params, ...req.body }).catch(err => {
@@ -30,8 +51,6 @@ class RoleValidator {
   }
 
   async list(req: Request) {
-    const nameIsArray = Array.isArray(req.query.name);
-
     const schema = Yup.object({
       name: Yup.lazy(val =>
         Array.isArray(val) ? Yup.array().of(Yup.string()).optional() : Yup.string().optional()
@@ -41,7 +60,7 @@ class RoleValidator {
       sort: Yup.string().oneOf(['asc', 'desc']).default('desc'),
     });
 
-    return schema.validate({ ...req.query, nameIsArray }).catch(err => {
+    return schema.validate({ ...req.query }).catch(err => {
       throw new ApiError(400, err.message);
     });
   }
